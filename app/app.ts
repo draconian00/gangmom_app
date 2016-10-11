2dee21a506dd1961ec0433e81651d3d5924d04c9
draconian00 <draconian00@gmail.com>
2016-10-11 19:38:27 +0900

    login.ts fix, add loading comment

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,7 @@
 
 import { Component } from '@angular/core';
 import { Platform, ionicBootstrap } from 'ionic-angular';
-import { StatusBar, InAppBrowser, Device } from 'ionic-native';
+import { StatusBar } from 'ionic-native';
 import { LoginPage } from './pages/login/login';
 
 
@@ -14,7 +14,6 @@ import { LoginPage } from './pages/login/login';
 })
 export class MyApp {
   public rootPage: any;
-  active_status: boolean = true;
 
   constructor(private platform: Platform) {
     this.rootPage = LoginPage;
@@ -23,54 +22,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-
-      // notification handler
-      var notificationHandler = (notification) => {
-        console.log('didReceiveRemoteNotifiactionCallBack: ' + JSON.stringify(notification));
-        let jsonData = notification.additionalData;
-        if (jsonData.redirect_url) {
-          this.active_status = notification.isActive;
-          let inapp = InAppBrowser.open(jsonData.redirect_url, '_blank', 'location=no,toolbar=no,zoom=no,hidden=yes');
-          inapp.addEventListener('loadstop', () => {
-            inapp.show();
-          });
-          // let open_confirm = confirm('리뷰 수정페이지로 이동하시겠습니까?');
-          // if (open_confirm) {
-          //   this.active_status = false;
-          //   InAppBrowser.open(jsonData.redirect_url, '_blank', 'location=no,toolbar=no,zoom=no');
-          // } else {
-          //   this.active_status = true;
-          // }
-        }
-      };
-
-      // setting onesignal
-      window['plugins'].OneSignal.enableInAppAlertNotification(true);
-      window['plugins'].OneSignal.enableSound(true);
-      window['plugins'].OneSignal.enableVibrate(true);
-      window['plugins'].OneSignal.setSubscription(true);
-      window['plugins'].OneSignal.enableInAppAlertNotification(true);
-      window['plugins'].OneSignal.enableNotificationsWhenActive(true);
-
-      // onesignal init
-      window['plugins'].OneSignal.init(
-        '5db18f75-1e72-486d-a0db-82f902784dea', 
-        {googleProjectNumber: '600441567099'},
-        notificationHandler
-      );
-
-      // get onesignal device token then => open inappbrowser 
-      window['plugins'].OneSignal.getIds((ids) => {
-        // test
-        // let url = 'http://localhost:3000/m?device_token=' + ids.userId;
-        let url = 'http://gangmom.kr/m?device_token=' + ids.userId;
-        if (this.active_status) {
-          let inapp = InAppBrowser.open(url, '_blank', 'location=no,toolbar=no,zoom=no,hidden=yes');
-          inapp.addEventListener('loadstop', () => {
-            inapp.show();
-          });
-        }
-      });
     });
   }
 }
--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -4,7 +4,7 @@
 */
 
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
 import { InAppBrowser } from 'ionic-native';
 
 import { Http } from '@angular/http';
@@ -17,26 +17,82 @@ import 'rxjs/add/operator/toPromise';
 })
 export class LoginPage {
   deviceToken: any;
+  active_status: boolean = true;
 
-  constructor(public navCtrl: NavController, private http: Http) {
-    setTimeout(() => {
-      window['plugins'].OneSignal.getIds((ids) => {
-        this.deviceToken = ids.userId;
-      });
-    }, 10);
-
+  constructor(public navCtrl: NavController, private http: Http, private platform: Platform) {
     // app.ts 에서  InAppBrowser를 못 열었을때 login.html 에서 InAppBrowser를 열 수 있는 버튼 소환
     setTimeout(() => {
-      document.getElementById('spinner').className = 'loadingSpinner hide';
-      document.getElementById('refresh_div').className = '';
+      this.loadingComment_hide();
+      this.refreshComment_show();
     }, 10000);
+
+    platform.ready().then(() => {
+      // notification handler
+      var notificationHandler = (notification) => {
+        // console.log('didReceiveRemoteNotifiactionCallBack: ' + JSON.stringify(notification));
+        let jsonData = notification.additionalData;
+        if (jsonData.redirect_url) {
+          this.refreshComment_hide();
+          this.loadingComment_show();
+          this.active_status = notification.isActive;
+          let inapp = InAppBrowser.open(jsonData.redirect_url, '_blank', 'location=no,toolbar=no,zoom=no,hidden=yes');
+          inapp.addEventListener('loadstop', () => {
+            inapp.show();
+            this.loadingComment_hide();
+            this.refreshComment_show();
+          });
+        }
+      };
+
+      // setting onesignal
+      window['plugins'].OneSignal.enableInAppAlertNotification(true);
+      window['plugins'].OneSignal.enableSound(true);
+      window['plugins'].OneSignal.enableVibrate(true);
+      window['plugins'].OneSignal.setSubscription(true);
+      window['plugins'].OneSignal.enableInAppAlertNotification(true);
+      window['plugins'].OneSignal.enableNotificationsWhenActive(true);
+
+      // onesignal init
+      window['plugins'].OneSignal.init(
+        '5db18f75-1e72-486d-a0db-82f902784dea', 
+        {googleProjectNumber: '600441567099'},
+        notificationHandler
+      );
+
+      // get onesignal device token then => open inappbrowser 
+      window['plugins'].OneSignal.getIds((ids) => {
+        this.deviceToken = ids.userId;
+        let url = 'http://gangmom.kr/m?device_token=' + this.deviceToken;
+        if (this.active_status) {
+          let inapp = InAppBrowser.open(url, '_blank', 'location=no,toolbar=no,zoom=no,hidden=yes');
+          inapp.addEventListener('loadstop', () => {
+            inapp.show();
+          });
+        }
+      });
+    });
   }
 
   // launch InAppBrowser
   openBrowser() {
-    // test
-    // let url = 'http://localhost:3000/m?device_token=' + this.deviceToken;
     let url = 'http://gangmom.kr/m?device_token=' + this.deviceToken;
     InAppBrowser.open(url, '_blank', 'location=no,toolbar=no,zoom=no');
+  }
+
+  loadingComment_show() {
+    document.getElementById('spinner').className = 'loadingSpinner';
+    document.getElementById('loading_comment').className = '';
+  }
+  loadingComment_hide() {
+    document.getElementById('spinner').className = 'loadingSpinner hide';
+    document.getElementById('loading_comment').className = 'hide';
+  }
+  refreshComment_show() {
+    document.getElementById('refresh_comment').className = '';
+    document.getElementById('refresh_btn').className = 'button button-default';
+  }
+  refreshComment_hide() {
+    document.getElementById('refresh_comment').className = 'hide';
+    document.getElementById('refresh_btn').className = 'hide';
   }
 }
