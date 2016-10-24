1b822f7820d74d27f559db9d36eca1244ba437cc
draconian00 <draconian00@gmail.com>
2016-10-24 17:50:41 +0900

    Enable native Keyboard accessory bar

--- app/app.ts
+++ app/app.ts
@@ -22,6 +22,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      window['cordova'].plugins.Keyboard.hideKeyboardAccessoryBar(false);
     });
   }
 }
